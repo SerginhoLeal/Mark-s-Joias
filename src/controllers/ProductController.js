e1e92cc769eab88cbb12e25f1175d0725f5a7dbe
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-02-25 10:43:48 -0300

    full-api

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,73 +1,73 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const authConfig = require('../config/auth');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
 
-// const Product = mongoose.model('Product');
+const Product = mongoose.model('Product');
 
-// function generateToken(params = {}){
-//     return jwt.sign(params, authConfig.secret, {
-//     expiresIn: 86400,
-//     });
-// }
+function generateToken(params = {}){
+    return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+    });
+}
 
-// module.exports = {
-//     async index(req, res){
-//         const {page = 1} = req.query;
-//         const products = await Product.paginate({},{page, limit:5});
-//         return res.json(products);
-//     },
+module.exports = {
+    async index(req, res){
+        const {page = 1} = req.query;
+        const products = await Product.paginate({},{page, limit:5});
+        return res.json(products);
+    },
 
-//     async login(req, res){
-//         const {email, password} = req.body;
+    async login(req, res){
+        const {email, password} = req.body;
 
-//         const user = await Product.findOne({email}).select('+password');
+        const user = await Product.findOne({email}).select('+password');
         
-//         if(!user)
-//             return res.status(400).send({error:'Email inexistente'});
+        if(!user)
+            return res.status(400).send({error:'Email inexistente'});
         
-//         if(!await bcrypt.compare(password, user.password))
-//             return res.status(400).send({error:'Senha invalida'});
+        if(!await bcrypt.compare(password, user.password))
+            return res.status(400).send({error:'Senha invalida'});
         
-//         user.password = undefined;
+        user.password = undefined;
 
-//         res.send({
-//             user,
-//             token:generateToken({id:user.id}),
-//         });
-//     },
+        res.send({
+            user,
+            token:generateToken({id:user.id}),
+        });
+    },
 
-//     async store(req, res){
-//         const {email} = req.body;
-//     try{
-//         if(await Product.findOne({email}))
-//             return res.status(400).send({error:'Email já em uso!'});
+    async store(req, res){
+        const {email} = req.body;
+    try{
+        if(await Product.findOne({email}))
+            return res.status(400).send({error:'Email já em uso!'});
 
-//         const user = await Product.create(req.body);
+        const user = await Product.create(req.body);
 
-//         user.password = undefined;
-//         return res.send({
-//             user,
-//             token:generateToken({id:user.id}),
-//         });
-//         }catch(err){
-//             return res.statusCode(400).send({error:'fail'});
-//         }
-//     },
-//     async show(req, res){
-//         const product = await Product.findById(req.params.id);
-//         return res.json(product);
-//     },
+        user.password = undefined;
+        return res.send({
+            user,
+            token:generateToken({id:user.id}),
+        });
+        }catch(err){
+            return res.statusCode(400).send({error:'fail'});
+        }
+    },
+    async show(req, res){
+        const product = await Product.findById(req.params.id);
+        return res.json(product);
+    },
 
 
-//     async update(req, res){
-//         const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true});
-//         return res.json(product);
-//     },
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        return res.json(product);
+    },
 
-//     async destroy(req,res){
-//         await Product.findByIdAndRemove(req.params.id);
-//         return res.send();
-//     },
+    async destroy(req,res){
+        await Product.findByIdAndRemove(req.params.id);
+        return res.send();
+    },
 
-// };
+};
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-// const express = require('express');
+const express = require('express');
 
-// const routes = express.Router();
+const routes = express.Router();
 
-// const ProductController = require('./controllers/ProductController');
+const ProductController = require('./controllers/ProductController');
 
-// routes.post('/login', ProductController.login);
-// routes.post('/register', ProductController.store);
-// routes.get('/all', ProductController.index);
-// routes.get('/read/:id', ProductController.show);
-// routes.put('/update/:id', ProductController.update);
-// routes.delete('/delete/:id', ProductController.destroy);
+routes.post('/login', ProductController.login);
+routes.post('/register', ProductController.store);
+routes.get('/all', ProductController.index);
+routes.get('/read/:id', ProductController.show);
+routes.put('/update/:id', ProductController.update);
+routes.delete('/delete/:id', ProductController.destroy);
 
-// module.exports = routes;
+module.exports = routes;
