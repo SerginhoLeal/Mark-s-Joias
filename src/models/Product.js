deeed2737781a7cca4b545af4aaf12d973d16ef2
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-02-25 10:41:42 -0300

    full-api

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
 
-const mongoosePaginate = require('mongoose-paginate');
+// const mongoosePaginate = require('mongoose-paginate');
 
-const ProductSchema = new mongoose.Schema({
-    nome:{
-        type: String,
-        required: true,
-    },
-    email:{
-        type: String,
-        required: true,
-    },
-    password:{
-        type: String,
-        required: true,
-        select:false,
-    },
-    description:{
-        type:String,
-        required:true,
-    },
-    data:{
-        type: Date,
-        default:Date.now,
-    }
-});
+// const ProductSchema = new mongoose.Schema({
+//     nome:{
+//         type: String,
+//         required: true,
+//     },
+//     email:{
+//         type: String,
+//         required: true,
+//     },
+//     password:{
+//         type: String,
+//         required: true,
+//         select:false,
+//     },
+//     description:{
+//         type:String,
+//         required:true,
+//     },
+//     data:{
+//         type: Date,
+//         default:Date.now,
+//     }
+// });
 
-ProductSchema.pre('save', async function(next){
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash;
-    next();
-})
+// ProductSchema.pre('save', async function(next){
+//     const hash = await bcrypt.hash(this.password, 10);
+//     this.password = hash;
+//     next();
+// })
 
-ProductSchema.plugin(mongoosePaginate);
+// ProductSchema.plugin(mongoosePaginate);
 
-mongoose.model('Product', ProductSchema);
+// mongoose.model('Product', ProductSchema);
