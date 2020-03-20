fcacd8a442af2b455d54f7b9ded0adb6dd9270a0
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-03-20 19:06:30 -0300

    My-api

--- src/controllers/Search.js
+++ src/controllers/Search.js
@@ -4,13 +4,13 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res){
-        const {descript} = req.query;
+        const {description} = req.query;
 
-        const desc = descript.split(',').map(no => no.trim());
+        const description = description.split(',').map(no => no.trim());
 
         const usuarios = await Product.find({
             description:{
-                $in:desc,
+                $in:description,
             },
         });
 
